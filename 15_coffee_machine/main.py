dcf015e032bb917679d680670ca368659c976a8e
Oksana <oxanachyrkova9@gmail.com>
2023-09-30 12:17:31 +0300

    coffee machine final

--- 15_coffee_machine/main.py
+++ 15_coffee_machine/main.py
@@ -1,34 +1,25 @@
 from data import MENU
 
 
-def check_resources(machine_resources, data, selected_drink):
+def check_resources(order_ingredients):
     """The function checks if there are enough resources in the coffee machine
-    for making a certain drink and return 1 or 0"""
-    drink = data[selected_drink]
-    if selected_drink == "espresso":
-        if (drink["ingredients"]["water"] <= machine_resources["water"] and
-                drink["ingredients"]["coffee"] <= machine_resources["coffee"]):
-            return 1
-        elif drink["ingredients"]["water"] >= machine_resources["water"]:
-            print("Sorry, there is no enough water.")
+    for making a certain drink and return 1 (True) or 0 (False)"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry, there is no enough {item}.")
             return 0
-        else:
-            print("Sorry, there is no enough coffee.")
-            return 0
-    else:
-        if (drink["ingredients"]["water"] <= machine_resources["water"] and
-                drink["ingredients"]["milk"] <= machine_resources["milk"] and
-                drink["ingredients"]["coffee"] <= machine_resources["coffee"]):
-            return 1
-        elif drink["ingredients"]["water"] >= machine_resources["water"]:
-            print("Sorry, there is no enough water.")
-            return 0
-        elif drink["ingredients"]["milk"] >= machine_resources["milk"]:
-            print("Sorry, there is no enough milk.")
-            return 0
-        else:
-            print("Sorry, there is no enough coffee.")
-            return 0
+    return 1
+
+
+def process_coins():
+    """The function process the inserted coins and returns the total."""
+    print("Please, insert coins.")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+    total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    return total
 
 
 def check_drink_price(user_choice, data):
@@ -41,39 +32,37 @@ def check_drink_price(user_choice, data):
         return data["cappuccino"]["cost"]
 
 
-def check_transaction(user_coins, drink_price, machine_money):
-    """The function checks if there are enough money for making a drink"""
-    if user_coins == drink_price:
-        machine_money += user_coins
-        return machine_money
-    if user_coins > drink_price:
-        #change = round(user_coins - drink_price)
-        machine_money += drink_price
-        return machine_money
-        #print(f"Here is ${change} in change.")
+def check_transaction(user_coins, drink_cost):
+    """The function checks if there are enough money for making a drink and return 1 (True) or 0 (False)"""
+    if user_coins >= drink_cost:
+        change = round(user_coins - drink_price, 2)
+        print(f"Here is ${change} in change.")
+        global money
+        money += drink_cost
+        return 1
+        # return machine_money
+    # if user_coins > drink_cost:
+    #     change = round(user_coins - drink_price)
+    #     machine_money += drink_cost
+    #     return machine_money
+    #     #print(f"Here is ${change} in change.")
     else:
+        print("Sorry that's not enough money. Money refunded.")
         return 0
 
 
-def check_change(user_coins, drink_price):
-    """The function counts and prints the amount of change"""
-    if user_coins > drink_price:
-        change = round(user_coins - drink_price)
-        print(f"Here is ${change} in change.")
+# def check_change(user_coins, drink_price):
+#     """The function counts and prints the amount of change"""
+#     if user_coins > drink_price:
+#         change = round(user_coins - drink_price)
+#         print(f"Here is ${change} in change.")
 
 
-def change_machine_resources(machine_resources, selected_drink, data):
+def change_machine_resources(selected_drink, machine_resources, data):
     """The function updates the resources of machine after making a drink"""
-    drink = data[selected_drink]
-    if selected_drink == "espresso":
-        machine_resources["water"] -= drink["ingredients"]["water"]
-        machine_resources["coffee"] -= drink["ingredients"]["coffee"]
-        return machine_resources
-    else:
-        machine_resources["water"] -= drink["ingredients"]["water"]
-        machine_resources["milk"] -= drink["ingredients"]["milk"]
-        machine_resources["coffee"] -= drink["ingredients"]["coffee"]
-        return machine_resources
+    for item in data:
+        machine_resources[item] -= data[item]
+    print(f"Here is your {selected_drink}. Enjoy!")
 
 
 turn_off = False
@@ -89,25 +78,18 @@ while not turn_off:
     milk = resources["milk"]
     coffee = resources["coffee"]
 
-    prompt = input("   What would you like? (espresso/latte/cappuccino): ").lower()
+    choice = input("   What would you like? (espresso/latte/cappuccino): ").lower()
 
-    if prompt == "report":
+    if choice == "report":
         print(f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}")
-    elif prompt == "off":
+    elif choice == "off":
         turn_off = True
     else:
-        if check_resources(resources, MENU, prompt) == 1:
-            print("Please, insert coins.")
-            quarters = int(input("How many quarters?: "))
-            dimes = int(input("How many dimes?: "))
-            nickles = int(input("How many nickles?: "))
-            pennies = int(input("How many pennies?: "))
-            total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
-            drink_price = check_drink_price(prompt, MENU)
-            if check_transaction(total, drink_price, money) > 0:
-                check_change(total, drink_price)
-                money += check_transaction(total, drink_price, money)
-                change_machine_resources(resources, prompt, MENU)
-                print(f"Here is your {prompt}. Enjoy!")
-            else:
-                print("Sorry that's not enough money. Money refunded.")
+        drink = MENU[choice]
+        if check_resources(drink["ingredients"]) == 1:
+            user_payment = process_coins()
+            drink_price = check_drink_price(choice, MENU)
+            if check_transaction(user_payment, drink_price) > 0:
+                #check_change(user_payment, drink_price)
+                #money += check_transaction(user_payment, drink_price, money)
+                change_machine_resources(choice, resources, drink["ingredients"])
