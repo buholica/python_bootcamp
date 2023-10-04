0661bf3ddbe3b1739f874cc1d1f1ed58152d98f7
Oksana <oxanachyrkova9@gmail.com>
2023-10-04 21:06:40 +0300

    17 quiz final

--- 17_quiz_project/question_model.py
+++ 17_quiz_project/question_model.py
@@ -3,7 +3,3 @@ class Question:
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
-
-
-# new_question = Question("text", "False")
-# print(new_question.text)
