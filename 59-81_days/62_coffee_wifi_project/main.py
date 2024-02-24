ae5c2740e59fa40515a2d3d968df107008b0c8a5
Oksana <oxanachyrkova9@gmail.com>
2024-02-24 11:11:25 +0200

    62_wifi_project

--- 59-81_days/62_coffee_wifi_project/main.py
+++ 59-81_days/62_coffee_wifi_project/main.py
@@ -1,40 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, URL
 import csv
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 
+# noinspection PyTypeChecker
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    cafe = StringField('Cafe name', validators=[DataRequired(message="This field is required.")])
+    location = StringField('Cafe Location on Google Maps (URL)',
+                           validators=[DataRequired(), URL(require_tld=True, message="Invalid URL")])
     open_time = StringField('Opening Time, e.g. 8AM', validators=[DataRequired()])
     close_time = StringField('Closing Time, e.g. 6PM', validators=[DataRequired()])
-    coffee_rating = SelectField('Coffee Rating', validators=[DataRequired()], choices=[('✘'), ('☕️'), ('☕️☕️'), ('☕️☕️☕️'), ('☕️☕️☕️☕️'), ('☕️☕️☕️☕️☕️')])
-    wifi_rating = SelectField('WIFI Rating', validators=[DataRequired()], choices=[('✘'),('💪'), ('💪💪'), ('💪💪💪'), ('💪💪💪💪'), ('💪💪💪💪💪')])
-    power_rating = SelectField('Power Socket Availability', validators=[DataRequired()], choices=[('✘'),('🔌'), ('🔌🔌'), ('🔌🔌🔌'), ('🔌🔌🔌🔌'), ('🔌🔌🔌🔌🔌')])
-    submit = SubmitField('Submit')
-
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
+    coffee_rating = SelectField('Coffee Rating', validators=[DataRequired(message="This field is required.")],
+                                choices=['✘', '☕️', '☕️☕️', '☕️☕️☕️', '☕️☕️☕️☕️', '☕️☕️☕️☕️☕️'])
+    wifi_rating = SelectField('WIFI Rating', validators=[DataRequired()],
+                              choices=['✘', '💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪'])
+    power_rating = SelectField('Power Socket Availability', validators=[DataRequired()],
+                               choices=['✘', '🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌'])
+    submit = SubmitField(label='Submit')
 
 
 # all Flask routes below
@@ -43,14 +33,14 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add',  methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        flash("The cafe was added successfully", "success")
+        with open("cafe-data.csv", 'a', encoding='utf-8') as csv_file:
+            csv_file.write(f"\n{form.cafe.data}, {form.location.data}, {form.open_time.data}, {form.close_time.data},"
+                           f"{form.coffee_rating.data}, {form.wifi_rating.data}, {form.power_rating.data}")
     return render_template('add.html', form=form)
 
 
